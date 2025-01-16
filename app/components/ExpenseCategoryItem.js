a6677aafc769cbe535ecabb17080b1d5394c8993
Kartik <kartikbhatia2304@gmail.com>
2025-01-16 12:55:00 +0530

    Created expense lists with category, title and amount with demo data, later on this will be fetched from the database.

---
+++ app/components/ExpenseCategoryItem.js
@@ -0,0 +1,15 @@
+import { currencyFormatter } from "@/app/lib/utils";
+function ExpenseCategoryItem({color, title, amount}) {
+    return (
+        <button>
+        <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+            <div className="flex items-center gap-2">
+                <div className="w-[25px] h-[25px] rounded-full" style={{backgroundColor: color}}></div>
+                <h4 className="capitalize">{title}</h4>
+            </div>
+            <p>{currencyFormatter(amount)}</p>
+        </div>
+        </button>
+    );
+}
+export default ExpenseCategoryItem;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,51 @@
+import { currencyFormatter } from "@/app/lib/utils";
+import ExpenseCategoryItem from "@/app/components/ExpenseCategoryItem";
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: '#000',
+    amount: 500
+  },
+  {
+    id: 2,
+    title: "Shopping",
+    color: '#fff',
+    amount: 2000
+  },
+  {
+    id: 3,
+    title: "Fuel",
+    color: '#f4f',
+    amount: 200
+  }
+];
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <main className="max-w-5xl px-6 mx-auto">
+      <section className="py-3">
+      <small className="text-gray-400 text-md">My Balance</small>
+      <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+      </section>
+
+      <section className="flex items-center gap-2 py-3">
+        <button className="btn btn-primary">+Expenses</button>
+        <button className="btn btn-primary-outline">+Income</button>
+      </section>
+
+      {/* Expenses */}
+      <section className="py-6">
+        <h3 className="text-2xl">My Expenses</h3>
+        <div className="flex flex-col gap-4 mt-6">
+          {/* Expense Items */}
+          {DUMMY_DATA.map(expense => {
+            return(
+              <ExpenseCategoryItem color={expense.color} title={expense.title} amount={expense.amount}></ExpenseCategoryItem>
+            );
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
